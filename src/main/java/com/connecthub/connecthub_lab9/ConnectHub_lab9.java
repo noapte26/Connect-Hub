7e06cb4f866b3f52070f0fb7b8d98c7cd96be361
noapte26 <pinkypiemuffin@gmail.com>
2024-12-02 09:01:30 +0200

    Hello ^^

---
+++ src/main/java/com/connecthub/connecthub_lab9/ConnectHub_lab9.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.connecthub.connecthub_lab9;
+
+/**
+ *
+ * @author hebai
+ */
+public class ConnectHub_lab9 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
